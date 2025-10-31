5c18284caff90b32b83c7b60d3a7f50fa8acbc6e
Marcelo Barros de Almeida <marcelo.barros@ufu.br>
2025-10-31 17:43:33 -0300

    Add TX power control and improve debug output
    Introduces set_tx_pwr function to configure transmit power with validation and error handling. Enhances debugging by printing TX/RX frames and callback events, updates default channel and PAN ID, and fixes module naming to 'ieee802154'.

--- ports/esp32/ieee802154.c
+++ ports/esp32/ieee802154.c
@@ -45,9 +45,11 @@
 
 #include "py/obj.h"
 #include "py/runtime.h"
+#include "py/mphal.h"
 
 #if MICROPY_PY_IEEE802154
 
+#define DBG_ON 1
 
 typedef struct ieee802154_ctrl_s
 {
@@ -57,16 +59,18 @@ typedef struct ieee802154_ctrl_s
     bool enabled;
     uint8_t seq_num;
     uint8_t tx[128];
+    uint8_t rx[128];
+    int8_t tx_pwr;
 } ieee802154_ctrl_t;
-static const char *TAG = "mp_ieee802154";
 
 static ieee802154_ctrl_t ieee802154_ctrl = 
 {
-    .channel = 11,
-    .panid = 1000,
+    .channel = 25,
+    .panid = 13,
     .short_addr = 10000,
     .enabled = false,
     .seq_num = 0,
+    .tx_pwr = 20,
 };
 
 static void ieee802154_check_if_enabled(void)
@@ -84,6 +88,10 @@ static mp_obj_t ieee802154_init(void)
         if(esp_ieee802154_enable() == ESP_OK)
         {
             ieee802154_ctrl.enabled = true;
+            esp_ieee802154_set_ack_timeout(2*16);
+            esp_ieee802154_set_promiscuous(false);
+            esp_ieee802154_set_rx_when_idle(true);
+            esp_ieee802154_set_coordinator(false);
         }
         else
         {
@@ -106,6 +114,29 @@ static mp_obj_t ieee802154_deinit(void)
         {
             mp_raise_msg(&mp_type_OSError, MP_ERROR_TEXT("Failed to disable IEEE 802.15.4"));
         }
+    }
+
+    return mp_const_none;
+}
+
+static mp_obj_t ieee802154_set_tx_pwr(mp_obj_t pwr_obj)
+{
+    ieee802154_check_if_enabled();
+
+    mp_int_t pwr = mp_obj_get_int(pwr_obj);
+
+    if (pwr < -15 || pwr > 20) 
+    {
+        mp_raise_msg(&mp_type_ValueError, MP_ERROR_TEXT("TX Power must be between -15 and 20 dBm"));
+    }
+
+    if(esp_ieee802154_set_txpower((int8_t)pwr) == ESP_OK)
+    {
+        ieee802154_ctrl.tx_pwr = (int8_t)pwr;
+    }
+    else
+    {
+        mp_raise_msg(&mp_type_OSError, MP_ERROR_TEXT("Failed to set TX Power"));
     }
 
     return mp_const_none;
@@ -216,14 +247,26 @@ static uint8_t ieee802154_get_next_seq_number(bool retry)
     return ieee802154_ctrl.seq_num;
 }
 
+void esp_ieee802154_receive_done(uint8_t *data, esp_ieee802154_frame_info_t *frame_info)
+{
+    #if DBG_ON == 1
+    mp_hal_stdout_tx_str("RX ");
+    for(size_t pos = 0 ; pos < data[0] ; pos++)
+        mp_printf(&mp_plat_print, "%02X", data[pos]);
+
+    mp_hal_stdout_tx_str("\n");
+    #endif
+
+}
+
 void esp_ieee802154_transmit_done(const uint8_t *frame, const uint8_t *ack, esp_ieee802154_frame_info_t *ack_frame_info)
 {
-    ESP_LOGI(TAG,"Transmit done callback called");
+    mp_hal_stdout_tx_str("==> Transmit done callback called\n");
 }
 
 void esp_ieee802154_transmit_failed(const uint8_t *frame, esp_ieee802154_tx_error_t error)
 {
-    ESP_LOGI(TAG,"Transmit failed callback called");
+    mp_printf(&mp_plat_print, "Transmit failed callback called %d\n",error);
 }
 
 static mp_obj_t ieee802154_send_msg(mp_obj_t payload_obj, mp_obj_t dst_addr_obj, mp_obj_t retry_obj)
@@ -245,7 +288,7 @@ static mp_obj_t ieee802154_send_msg(mp_obj_t payload_obj, mp_obj_t dst_addr_obj,
     }
 
     // full size: FC(2)+SN(1)+DPANID(2)+DADDR(2)+SADDR(2) + PAYLOAD + FCS(2)
-    ieee802154_ctrl.tx[0] = len + 9; // FC(2)+SN(1)+DPANID(2)+DADDR(2)+SADDR(2)
+    ieee802154_ctrl.tx[0] = len + 11; // FC(2)+SN(1)+DPANID(2)+DADDR(2)+SADDR(2)
     ieee802154_ctrl.tx[1] = 0x61; // Frame Control: Data Frame, Ack Request, PAN ID Compression
     ieee802154_ctrl.tx[2] = 0x88; // Frame Control: Src and Dst Addr are short (16 bit)
     ieee802154_ctrl.tx[3] = ieee802154_get_next_seq_number(retry);
@@ -258,12 +301,22 @@ static mp_obj_t ieee802154_send_msg(mp_obj_t payload_obj, mp_obj_t dst_addr_obj,
 
     memcpy(&ieee802154_ctrl.tx[10], payload, len);
 
-    if(esp_ieee802154_transmit(ieee802154_ctrl.tx, true) != ESP_OK)
-    {
-        mp_raise_msg(&mp_type_OSError, MP_ERROR_TEXT("Failed to transmit message"));
-    }
+    #if DBG_ON == 1
+    mp_hal_stdout_tx_str("TX ");
+    for(size_t pos = 0 ; pos < (len+10) ; pos++)
+        mp_printf(&mp_plat_print, "%02X", ieee802154_ctrl.tx[pos]);
 
-    return mp_const_none;
+    mp_hal_stdout_tx_str("\n");
+    #endif
+
+    esp_err_t status = esp_ieee802154_transmit(ieee802154_ctrl.tx, true);
+
+    if(status != ESP_OK)
+    {
+        mp_printf(&mp_plat_print,"=> %s\n",esp_err_to_name(status));
+     }
+
+    return mp_obj_new_bool(status == ESP_OK);
 }
 
 static MP_DEFINE_CONST_FUN_OBJ_0(ieee802154_init_obj, ieee802154_init);
@@ -275,9 +328,10 @@ static MP_DEFINE_CONST_FUN_OBJ_0(ieee802154_get_panid_obj, ieee802154_get_panid)
 static MP_DEFINE_CONST_FUN_OBJ_1(ieee802154_set_short_addr_obj, ieee802154_set_short_addr);
 static MP_DEFINE_CONST_FUN_OBJ_0(ieee802154_get_short_addr_obj, ieee802154_get_short_addr);
 static MP_DEFINE_CONST_FUN_OBJ_3(ieee802154_send_msg_obj, ieee802154_send_msg);
+static MP_DEFINE_CONST_FUN_OBJ_1(ieee802154_set_tx_pwr_obj, ieee802154_set_tx_pwr);
 
 static const mp_rom_map_elem_t ieee802154_module_globals_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_ieee154) },
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_ieee802154) },
     { MP_ROM_QSTR(MP_QSTR_init), MP_ROM_PTR(&ieee802154_init_obj) },
     { MP_ROM_QSTR(MP_QSTR_deinit), MP_ROM_PTR(&ieee802154_deinit_obj) },
     { MP_ROM_QSTR(MP_QSTR_set_channel), MP_ROM_PTR(&ieee802154_set_channel_obj) },
@@ -287,6 +341,7 @@ static const mp_rom_map_elem_t ieee802154_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_set_short_addr), MP_ROM_PTR(&ieee802154_set_short_addr_obj) },
     { MP_ROM_QSTR(MP_QSTR_get_short_addr), MP_ROM_PTR(&ieee802154_get_short_addr_obj) },
     { MP_ROM_QSTR(MP_QSTR_send_msg), MP_ROM_PTR(&ieee802154_send_msg_obj) },
+    { MP_ROM_QSTR(MP_QSTR_set_tx_pwr), MP_ROM_PTR(&ieee802154_set_tx_pwr_obj) },
 };
 
 static MP_DEFINE_CONST_DICT(ieee802154_module_globals, ieee802154_module_globals_table);
@@ -297,7 +352,7 @@ const mp_obj_module_t ieee802154_user_cmodule = {
 };
 
 // Registro no MicroPython
-MP_REGISTER_MODULE(MP_QSTR_ieee154, ieee802154_user_cmodule);
+MP_REGISTER_MODULE(MP_QSTR_ieee802154, ieee802154_user_cmodule);
 
 
 #endif // MICROPY_PY_IEEE802154
